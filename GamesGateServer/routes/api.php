387937b1a57b6fc3df64a13ff9e2533b2a06af3b
Kagaya2004 <kaykyogaya@gmail.com>
2025-04-23 14:39:16 -0300

    API e Migrations
    Instalação da API;
    Criação da Migration SoftDelete

---
+++ GamesGateServer/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UsuarioController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/user/index', [UsuarioController::class, 'index']);
+Route::get('/user/show/{id}', [UsuarioController::class, 'show']);
+Route::post('/user/store', [UsuarioController::class, 'store']);
+Route::delete('/user/destroy/{id}', [UsuarioController::class, 'destroy']);
