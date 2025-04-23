dfa3a7785be3e6e4bf5710df800b8916fadf2321
Kagaya2004 <kaykyogaya@gmail.com>
2025-04-23 13:54:42 -0300

    Dia 23/04/25
    1° Setup:
    Criação dos Models (Configuração do Model User);
    Criação dos Controllers;
    Criação do Projeto React;

--- GamesGateServer/app/Models/User.php
+++ GamesGateServer/app/Models/User.php
@@ -5,13 +5,12 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'login',
+        'dataNascimento',
+        'dataCriacao',
+        'dataLogin',
+        'descricao',
+        'status',
     ];
 
     /**
@@ -32,7 +37,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'update_at',
+        'updated_at',
         'created_at',
         'deleted_at',
     ];
