89bc46ae317d23d0ee059a6f43ca2c47ced03656
Kagaya2004 <kaykyogaya@gmail.com>
2025-04-29 16:59:34 -0300

    Correção das Páginas CRUD e Rotas

--- GamesGateServer/resources/js/app/src/routes/Rotas.jsx
+++ GamesGateServer/resources/js/app/src/routes/Rotas.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
- import { Routes, Route } from 'react-router-dom'
- import UsuarioFormList from '../views/user/UserFormList'
- import UsuarioFormStore from '../views/user/UserFormStore'
- import UsuarioFormUpdate from '../views/user/UserFormUpdate'
- import UsuarioFormShow from '../views/user/UserFormShow'
- import UsuarioFormDestroy from '../views/user/UserFormDestroy'
+import { Routes, Route } from 'react-router-dom'
+import UsuarioFormList from '../views/Usuario/UsuarioFormList'
+import UsuarioFormStore from '../views/Usuario/UsuarioFormStore'
+import UsuarioFormUpdate from '../views/Usuario/UsuarioFormUpdate'
+import UsuarioFormShow from '../views/Usuario/UsuarioFormShow'
+import UsuarioFormDestroy from '../views/Usuario/UsuarioFormDestroy'
  
- 
- const Rotas = () => {
-   return (
-     <Routes>
-         <Route path="/user/index" element={<UsuarioFormList />} />
-         <Route path="/user/store" element={<UsuarioFormStore />} />
-         <Route path="/user/update" element={<UsuarioFormUpdate />} />
-         <Route path="/user/show" element={<UsuarioFormShow />} />
-         <Route path="/user/destroy" element={<UsuarioFormDestroy />} />
-     </Routes>
-   )
- }
- 
- export default Rotas
+const Rotas = () => {
+return (
+    <Routes>
+        <Route path="/user/index" element={<UsuarioFormList/>} />
+        <Route path="/user/store" element={<UsuarioFormStore/>} />
+        <Route path="/user/update" element={<UsuarioFormUpdate/>} />
+        <Route path="/user/show" element={<UsuarioFormShow/>} />
+        <Route path="/user/destroy" element={<UsuarioFormDestroy/>} />
+    </Routes>
+)
+}
+
+export default Rotas
