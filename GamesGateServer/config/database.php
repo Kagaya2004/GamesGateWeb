e8f3d904e604d879b7ae4f7a2b4770835e1b6221
Kagaya2004 <kaykyogaya@gmail.com>
2025-04-29 18:22:46 -0300

    Update database.php

--- GamesGateServer/config/database.php
+++ GamesGateServer/config/database.php
@@ -47,7 +47,7 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3307'),
-            'database' => env('DB_DATABASE', 'gamesgateapi'),
+            'database' => env('DB_DATABASE', 'gamesgateserver'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
