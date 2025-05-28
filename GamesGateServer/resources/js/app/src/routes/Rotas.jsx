6b58ae20ef4d3851c272e35df2fc9e518edad808
Kagaya2004 <kaykyogaya@gmail.com>
2025-05-28 20:54:32 -0300

    Atualização da Api e Rotas

--- GamesGateServer/resources/js/app/src/routes/Rotas.jsx
+++ GamesGateServer/resources/js/app/src/routes/Rotas.jsx
@@ -14,6 +14,21 @@ return (
         <Route path="/usuario/update/:id" element={<UsuarioFormUpdate/>} />
         <Route path="/usuario/show/:id" element={<UsuarioFormShow/>} />
         <Route path="/usuario/destroy/:id" element={<UsuarioFormDestroy/>} />
+        <Route path="/desenvolvedora/index" element={<DesenvolvedoraFormList/>} />
+        <Route path="/desenvolvedora/store" element={<DesenvovledoraFormStore/>} />
+        <Route path="/desenvolvedora/update/:id" element={<DesenvolvedoraFormUpdate/>} />
+        <Route path="/desenvolvedora/show/:id" element={<DesenvolvedoraFormShow/>} />
+        <Route path="/desenvolvedora/destroy/:id" element={<DesenvolvedoraFormDestroy/>} />
+        <Route path="/jogo/index" element={<JogoFormList/>} />
+        <Route path="/jogo/store" element={<JogoFormStore/>} />
+        <Route path="/jogo/update/:id" element={<JogoFormUpdate/>} />
+        <Route path="/jogo/show/:id" element={<JogoFormShow/>} />
+        <Route path="/jogo/destroy/:id" element={<JogoFormDestroy/>} />
+        <Route path="/genero/index" element={<GeneroFormList/>} />
+        <Route path="/genero/store" element={<GeneroFormStore/>} />
+        <Route path="/genero/update/:id" element={<GeneroFormUpdate/>} />
+        <Route path="/genero/show/:id" element={<GeneroFormShow/>} />
+        <Route path="/genero/destroy/:id" element={<GeneroFormDestroy/>} />
     </Routes>
 )
 }
--- GamesGateServer/routes/api.php
+++ GamesGateServer/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\DesenvolvedoraController;
 
 Route::get('/usuario', function (Request $request) {
     return $request->usuario();
@@ -14,4 +15,25 @@ Route::prefix('/usuario')->group(function(){
     Route::post('/store', [UsuarioController::class, 'store']);
     Route::put('/update/{id}', [UsuarioController::class, 'update']);
     Route::delete('/destroy/{id}', [UsuarioController::class, 'destroy']);
+});
+Route::prefix('/desenvolvedora')->group(function(){
+    Route::get('/index', [DesenvolvedoraController::class, 'index']);
+    Route::get('/show/{id}', [DesenvolvedoraController::class, 'show']);
+    Route::post('/store', [DesenvolvedoraController::class, 'store']);
+    Route::put('/update/{id}', [DesenvolvedoraController::class, 'update']);
+    Route::delete('/destroy/{id}', [DesenvolvedoraController::class, 'destroy']);
+});
+Route::prefix('/jogo')->group(function(){
+    Route::get('/index', [JogoController::class, 'index']);
+    Route::get('/show/{id}', [JogoController::class, 'show']);
+    Route::post('/store', [JogoController::class, 'store']);
+    Route::put('/update/{id}', [JogoController::class, 'update']);
+    Route::delete('/destroy/{id}', [JogoController::class, 'destroy']);
+});
+Route::prefix('/genero')->group(function(){
+    Route::get('/index', [GeneroController::class, 'index']);
+    Route::get('/show/{id}', [GeneroController::class, 'show']);
+    Route::post('/store', [GeneroController::class, 'store']);
+    Route::put('/update/{id}', [GeneroController::class, 'update']);
+    Route::delete('/destroy/{id}', [GeneroController::class, 'destroy']);
 });
